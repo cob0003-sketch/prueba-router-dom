119ca96cc40462d1c7b9211851561ef64d50e880
Carlos Orihuela <caorihuela1975@gmail.com>
2025-10-29 20:14:57 +0100

    Actualizar rutas para gitPage

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,38 @@
-import { createBrowserRouter } from "react-router-dom"
-import Layout from "./layouts/Layout"
-import IndexPage from "./views/IndexPage"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import IndexPage, { action as actionDataForm } from './views/IndexPage'
+import Desarrollo from './views/DesarrolloPage'
 
-import Desarrollo from "./views/DesarrolloPage"
-import {action as actionDataForm } from './views/IndexPage'
-
-export const routerApp = createBrowserRouter( 
-
-    [
-        {
-            path: '/',
-            element: <Layout />,
-            children: [
-                {
-                    index: true,
-                    element:<IndexPage/>,
-                    action: actionDataForm
-                },
-                  {
-                    path:'desarrollo',
-                    element: <Desarrollo />
-                }
-            ]
-        }
+const routes = [
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <IndexPage />,
+        action: actionDataForm
+      },
+      {
+        path: 'desarrollo',
+        element: <Desarrollo />
+      }
     ]
+  }
+]
+
+export function AppRouter() {
+  const element = useRoutes(routes)
+  return element
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HashRouter>
+      <AppRouter />
+    </HashRouter>
+  </StrictMode>
 )
